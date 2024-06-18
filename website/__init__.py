445242929bc5eb3d74a48ec2fa5036bae720aa0b
KirillBad <37250878+KirillBad@users.noreply.github.com>
2024-06-18 08:14:23 +0300

    Add files via upload

--- website/__init__.py
+++ website/__init__.py
@@ -1,29 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_migrate import Migrate
+
 
 db = SQLAlchemy()
-migrate = Migrate()
+
 
 def create_app():
     from . import config
-    app = Flask(__name__)
-    UPLOAD_FOLDER = 'website/static/covers'
-    app.config["SECRET_KEY"] = config.SECRET_KEY
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}'
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-    db.init_app(app)
-    migrate.init_app(app, db)
 
+    app = Flask(__name__)
+    UPLOAD_FOLDER = "website/static/covers"
+    app.config["SECRET_KEY"] = config.SECRET_KEY
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        f"mysql://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}"
+    )
+    app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+    db.init_app(app)
 
     from .auth import auth
     from .views import views
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
-    from .models import User, Role
+    from .models import User
 
     create_db(app)
 
@@ -36,6 +37,7 @@ def create_app():
 
     return app
 
+
 def create_db(app):
     with app.app_context():
-       db.create_all()
+        db.create_all()
--- website/views.py
+++ website/views.py
@@ -1,4 +1,13 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for, current_app, jsonify
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    flash,
+    redirect,
+    url_for,
+    current_app,
+    jsonify,
+)
 from flask_login import login_required, logout_user, current_user
 from werkzeug.utils import secure_filename
 from .models import Book, Style, Cover, Review
@@ -6,78 +15,113 @@ import os, hashlib, markdown, bleach
 from . import decorators
 from . import db
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg"])
+
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 def commit_book_data(request, id=None):
-    title = request.form.get('title')
-    style = request.form.getlist('style')
-    short_description = bleach.clean(request.form.get('short_description'))
-    year = request.form.get('year')
-    publisher = request.form.get('publisher')
-    author = request.form.get('author')
-    pages = request.form.get('pages')
+    title = request.form.get("title")
+    style = request.form.getlist("style")
+    short_description = bleach.clean(request.form.get("short_description"))
+    year = request.form.get("year")
+    publisher = request.form.get("publisher")
+    author = request.form.get("author")
+    pages = request.form.get("pages")
 
-    if not all([title, style, year, publisher, author, pages, short_description != 'Краткое описание']):
+    if not all(
+        [
+            title,
+            style,
+            year,
+            publisher,
+            author,
+            pages,
+            short_description != "Краткое описание",
+        ]
+    ):
         flash("Пожалуйста, введите все данные", category="error")
         return redirect(request.url)
 
-    if 'file' in request.files:
-        file = request.files['file']
-        if file.filename == '':
+    if "file" in request.files:
+        file = request.files["file"]
+        if file.filename == "":
             flash("Пожалуйста, введите все данные", category="error")
             return redirect(request.url)
-        if file and allowed_file(file.filename):   
-            file_md5 = hashlib.md5(file.read()).hexdigest() 
+        if file and allowed_file(file.filename):
+            file_md5 = hashlib.md5(file.read()).hexdigest()
             filename = secure_filename(file.filename)
             try:
-                cover = db.session.scalars(db.select(Cover).where(Cover.MD5_hash == file_md5)).one_or_none()
+                cover = db.session.scalars(
+                    db.select(Cover).where(Cover.MD5_hash == file_md5)
+                ).one_or_none()
                 if cover:
                     cover_id = cover.id
                 else:
-                    new_cover = Cover(file_name=filename, MIME_type=file.content_type, MD5_hash=file_md5)
+                    new_cover = Cover(
+                        file_name=filename,
+                        MIME_type=file.content_type,
+                        MD5_hash=file_md5,
+                    )
                     db.session.add(new_cover)
-                    db.session.flush() 
+                    db.session.flush()
                     cover_id = new_cover.id
-                new_book = Book(title=title, short_description=short_description, year=year, publisher=publisher, author=author, pages=pages, cover_id=cover_id)
-                styles = db.session.scalars(db.select(Style).where(Style.id.in_(style))).all()
+                new_book = Book(
+                    title=title,
+                    short_description=short_description,
+                    year=year,
+                    publisher=publisher,
+                    author=author,
+                    pages=pages,
+                    cover_id=cover_id,
+                )
+                styles = db.session.scalars(
+                    db.select(Style).where(Style.id.in_(style))
+                ).all()
                 new_book.styles.extend(styles)
                 db.session.add(new_book)
                 db.session.commit()
                 book_id = new_book.id
                 file.seek(0)
-                file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+                file.save(os.path.join(current_app.config["UPLOAD_FOLDER"], filename))
 
-                return redirect(url_for('views.view_book', id=book_id))
+                return redirect(url_for("views.view_book", id=book_id))
             except Exception as e:
                 db.session.rollback()
-                flash("При сохранении данных возникла ошибка. Проверьте корректность введённых данных.", category="error")   
+                flash(
+                    "При сохранении данных возникла ошибка. Проверьте корректность введённых данных.",
+                    category="error",
+                )
                 return redirect(request.url)
     else:
         book = Book.query.get_or_404(id)
-        book.title = request.form.get('title')
-        book.style = request.form.getlist('style')
-        book.short_description = bleach.clean(request.form.get('short_description'))
-        book.year = request.form.get('year')
-        book.publisher = request.form.get('publisher')
-        book.author = request.form.get('author')
-        book.pages = request.form.get('pages')
+        book.title = request.form.get("title")
+        book.style = request.form.getlist("style")
+        book.short_description = bleach.clean(request.form.get("short_description"))
+        book.year = request.form.get("year")
+        book.publisher = request.form.get("publisher")
+        book.author = request.form.get("author")
+        book.pages = request.form.get("pages")
         try:
             db.session.commit()
         except Exception as e:
             db.session.rollback()
-            flash("При сохранении данных возникла ошибка. Проверьте корректность введённых данных.", category="error")   
-            return redirect(request.url)      
+            flash(
+                "При сохранении данных возникла ошибка. Проверьте корректность введённых данных.",
+                category="error",
+            )
+            return redirect(request.url)
 
-@views.route('/')
+
+@views.route("/")
 def home_page():
     books = Book.query.order_by(Book.year.desc())
 
-    page = request.args.get('page')
+    page = request.args.get("page")
 
     if page and page.isdigit():
         page = int(page)
@@ -88,57 +132,81 @@ def home_page():
 
     return render_template("home.html", user=current_user, pages=pages)
 
-@views.route('/add_book', methods=['GET', 'POST'])
-@decorators.role_required('admin')
-def add_book():                 
-    if request.method == 'POST':
+
+@views.route("/add_book", methods=["GET", "POST"])
+@decorators.role_required("admin")
+def add_book():
+    if request.method == "POST":
         return commit_book_data(request)
 
     styles = db.session.scalars(db.select(Style)).all()
 
     return render_template("add_book.html", user=current_user, book=None, styles=styles)
 
-@views.route('/view_book/<int:id>', methods=['GET', 'POST'])
-@decorators.role_required('admin', 'mod', 'user')
+
+@views.route("/view_book/<int:id>", methods=["GET", "POST"])
+@decorators.role_required("admin", "mod", "user")
 def view_book(id):
     book = Book.query.get_or_404(id)
     book.short_description = markdown.markdown(book.short_description)
 
     styles = db.session.scalars(db.select(Style)).all()
 
-    reviews = db.session.scalars(db.select(Review).where(Review.book_id == id)).all()
-    user_reviewed = False
+    reviews = db.session.scalars(
+        db.select(Review).where(Review.book_id == id, Review.status == "accepted")
+    ).all()
 
-    for review in reviews:
-        review.text = markdown.markdown(review.text)
-        if review.user_id == current_user.id:
-            user_reviewed = True
+    user_reviewed = db.session.scalars(
+        db.select(Review).where(Review.user_id == current_user.id, Review.book_id == id)
+    ).all()
 
-    return render_template("view_book.html", user=current_user, book=book, styles=styles, reviews=reviews, user_reviewed=user_reviewed)
+    print(user_reviewed)
 
-@views.route('/edit_book/<id>', methods=['GET', 'POST'])
-@decorators.role_required('admin', 'mod')
+    # for review in reviews:
+    #     review.text = markdown.markdown(review.text)
+    #     if review.user_id == current_user.id:
+    #         user_reviewed = True
+
+    return render_template(
+        "view_book.html",
+        user=current_user,
+        book=book,
+        styles=styles,
+        reviews=reviews,
+        user_reviewed=user_reviewed,
+    )
+
+
+@views.route("/edit_book/<id>", methods=["GET", "POST"])
+@decorators.role_required("admin", "mod")
 def edit_book(id):
     book = Book.query.get_or_404(id)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         commit_book_data(request, id)
-        return redirect(url_for('views.home_page'))
+        return redirect(url_for("views.home_page"))
     else:
         styles = db.session.scalars(db.select(Style)).all()
-        return render_template("edit_book.html", user=current_user, book=book, styles=styles)
+        return render_template(
+            "edit_book.html", user=current_user, book=book, styles=styles
+        )
 
-@views.route('/delete_book/<id>', methods=['GET', 'POST'])
-@decorators.role_required('admin')
+
+@views.route("/delete_book/<id>", methods=["GET", "POST"])
+@decorators.role_required("admin")
 def delete_book(id):
     book = Book.query.get_or_404(id)
 
     try:
         cover = book.cover
-        cover_count = db.session.scalars(db.select(Book).where(cover.id == Book.cover_id)).all()
+        cover_count = db.session.scalars(
+            db.select(Book).where(cover.id == Book.cover_id)
+        ).all()
         db.session.delete(book)
         if cover and len(cover_count) == 1:
-            file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], cover.file_name)
+            file_path = os.path.join(
+                current_app.config["UPLOAD_FOLDER"], cover.file_name
+            )
             if os.path.exists(file_path):
                 os.remove(file_path)
             db.session.delete(cover)
@@ -147,12 +215,13 @@ def delete_book(id):
 
     except Exception as e:
         db.session.rollback()
-        flash(f'Error occured: {e}', category='error')
+        flash(f"Error occured: {e}", category="error")
 
-    return redirect(url_for('views.home_page'))
+    return redirect(url_for("views.home_page"))
 
-@views.route('/add_review/<id>', methods=['GET', 'POST'])
-@decorators.role_required('admin', 'mod', 'user')
+
+@views.route("/add_review/<id>", methods=["GET", "POST"])
+@decorators.role_required("admin", "mod", "user")
 def add_review(id):
     book = Book.query.get_or_404(id)
 
@@ -160,26 +229,78 @@ def add_review(id):
 
     for review in reviews:
         if review.user_id == current_user.id:
-            flash('Вы уже оставляли отзыв', category='error')
-            return redirect(url_for('views.home_page'))
+            flash("Вы уже оставляли отзыв", category="error")
+            return redirect(url_for("views.home_page"))
 
-    if request.method == 'POST':
+    if request.method == "POST":
         book_id = id
         user_id = current_user.id
-        score = request.form.get('score')
-        text = bleach.clean(request.form.get('text'))
+        score = request.form.get("score")
+        text = bleach.clean(request.form.get("text"))
 
-        if text == 'Напишите свой отзыв': 
+        if text == "Напишите свой отзыв":
             flash("Пожалуйста, введите все данные", category="error")
-            return redirect(request.url) 
+            return redirect(request.url)
 
         try:
-            new_review = Review(book_id=book_id, user_id=user_id, score=score, text=text)
+            new_review = Review(
+                book_id=book_id, user_id=user_id, score=score, text=text
+            )
             db.session.add(new_review)
             db.session.commit()
-            return redirect(url_for('views.view_book', id=book_id))
+            flash(" Успешно! Ваш отзыв на проверке", category="success")
+            return redirect(url_for("views.view_book", id=book_id))
         except Exception as e:
             db.session.rollback()
-            flash(f'Error occured: {e}', category='error')
+            flash(f"Error occured: {e}", category="error")
 
-    return render_template('add_review.html', user=current_user, book=book)
+    return render_template("add_review.html", user=current_user, book=book)
+
+
+@views.route("/moderating_reviews", methods=["GET", "POST"])
+@decorators.role_required("mod")
+def moderating_reviews():
+    status = request.args.get("status")
+    book_id = request.args.get("book_id")
+    user_id = request.args.get("user_id")
+
+    if status and book_id and user_id:
+
+        moderated_review = db.session.scalars(
+            db.select(Review).where(
+                Review.book_id == book_id, Review.user_id == user_id
+            )
+        ).one_or_none()
+
+        if status == "declined":
+            db.session.delete(moderated_review)
+        else:
+            moderated_review.status = status
+
+        try:
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            flash(
+                f"{e}",
+                category="error",
+            )
+
+    reviews = db.session.scalars(
+        db.select(Review).where(Review.status == "pending")
+    ).all()
+    return render_template(
+        "moderating_reviews.html", reviews=reviews, user=current_user
+    )
+
+
+@views.route("/my_reviews", methods=["GET"])
+@login_required
+def my_reviews():
+    reviews = db.session.scalars(
+        db.select(Review).where(
+            Review.user_id == current_user.id, Review.status == "pending"
+        )
+    ).all()
+
+    return render_template("my_reviews.html", reviews=reviews, user=current_user)
